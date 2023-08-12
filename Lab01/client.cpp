7c70270bb4d79c3cfe108462db96e6b619547af9
JustADude29 <pavankumar82903@gmail.com>
2023-08-12 19:05:14 +0530

    First question works

---
+++ Lab01/client.cpp
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <string>
+#include <iostream>
+
+static const unsigned char base64_table[65] =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+char* base64_encode(unsigned char *src, size_t len)
+{
+    unsigned char *out, *pos;
+    const unsigned char *end, *in;
+
+    size_t olen;
+
+    olen = 4*((len + 2) / 3); /* 3-byte blocks to 4-byte */
+
+    char* outStr=new char[0];
+
+    if (olen < len)
+        return NULL; /* integer overflow */
+
+    outStr = new char[olen];
+    out = (unsigned char*)&outStr[0];
+
+    end = src + len;
+    in = src;
+    pos = out;
+    while (end - in >= 3) {
+        *pos++ = base64_table[in[0] >> 2];
+        *pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
+        *pos++ = base64_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
+        *pos++ = base64_table[in[2] & 0x3f];
+        in += 3;
+    }
+
+    if (end - in) {
+        *pos++ = base64_table[in[0] >> 2];
+        if (end - in == 1) {
+            *pos++ = base64_table[(in[0] & 0x03) << 4];
+            *pos++ = '=';
+        }
+        else {
+            *pos++ = base64_table[((in[0] & 0x03) << 4) |
+                (in[1] >> 4)];
+            *pos++ = base64_table[(in[1] & 0x0f) << 2];
+        }
+        *pos++ = '=';
+    }
+
+    return outStr;
+}
+
+#define MSG_LEN 1024
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <Server_IP_Address> <Server_Port_Number>\n", argv[0]);
+        return 1;
+    }
+
+    const char *serverIP = argv[1];
+    int serverPort = atoi(argv[2]);
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        perror("socket");
+        return 1;
+    }
+
+    struct sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(serverPort);
+    serverAddr.sin_addr.s_addr = inet_addr(serverIP);
+
+    if (connect(sock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
+        perror("connect");
+        return 1;
+    }
+
+    printf("Connected to server\n");
+    char buffer[MSG_LEN];
+    int bytesRead = recv(sock, buffer, sizeof(buffer), 0);
+    std::cout<<buffer<<std::endl;
+    if (bytesRead > 0) {
+        buffer[bytesRead] = '\0';
+        printf("Server Acknowledgment: %s\n", buffer);
+    }
+
+    while (1) {
+        char message[MSG_LEN];
+        printf("Enter a message, format:<msg_type> <msg>: ");
+        fgets(message, MSG_LEN, stdin);
+
+        char *formattedMessage = base64_encode((unsigned char *)message, strlen(message));
+
+        send(sock, formattedMessage, strlen(formattedMessage), 0);
+        if(message[0]=='3') break;
+    }
+
+    close(sock);
+    printf("Connection closed\n");
+
+    return 0;
+}
---
+++ Lab01/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+
+    // REP(i,0,res.size()) n[i]=res[i];
+    // LOG(b64decode(n, strlen(n)));
